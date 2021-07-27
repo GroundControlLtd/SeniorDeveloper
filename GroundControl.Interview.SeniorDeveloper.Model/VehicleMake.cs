6cc51eb56ab11713340e6dd64c984f905a78d98f
Conor Maher <conor-995@hotmail.co.uk>
2021-07-27 18:47:12 +0100

    Web UI Base Implementation
    - Implement dropdowns
    - Implement HTTP Request to API
    - Cascading effect not quite there yet

--- GroundControl.Interview.SeniorDeveloper.Model/VehicleMake.cs
+++ GroundControl.Interview.SeniorDeveloper.Model/VehicleMake.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-namespace GroundControl.Interview.SeniorDeveloper.Model
+﻿namespace GroundControl.Interview.SeniorDeveloper.Model
 {
+    // ToDo - really bad seperation of models as Data, Api and Web all share the same model
+    // If time - create separate models and mappers for these
     public class VehicleMake
     {
         public int Id { get; set; }
--- GroundControl.Interview.SeniorDeveloper.Web/Startup.cs
+++ GroundControl.Interview.SeniorDeveloper.Web/Startup.cs
@@ -23,6 +23,7 @@ namespace GroundControl.Interview.SeniorDeveloper.Web
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<VehicleService>();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
