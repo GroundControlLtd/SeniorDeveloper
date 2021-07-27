64ba5804d69abc9e725685c7fb6fd33c5aa6d8eb
Conor Maher <conor-995@hotmail.co.uk>
2021-07-27 19:12:13 +0100

    Web UI Amendments
    - Cascading effect now working
    - JsonPropertyName Attribute addition to models to facilitate deserialisation
    - Shared generic helper function for HTTP Requests
    - Minimal styling changes to space things out a little (no CSS as yet)

--- GroundControl.Interview.SeniorDeveloper.Model/VehicleMake.cs
+++ GroundControl.Interview.SeniorDeveloper.Model/VehicleMake.cs
@@ -1,10 +1,15 @@
-﻿namespace GroundControl.Interview.SeniorDeveloper.Model
+﻿using System.Text.Json.Serialization;
+
+namespace GroundControl.Interview.SeniorDeveloper.Model
 {
     // ToDo - really bad seperation of models as Data, Api and Web all share the same model
     // If time - create separate models and mappers for these
     public class VehicleMake
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
     }
 }
--- GroundControl.Interview.SeniorDeveloper.Model/VehicleModel.cs
+++ GroundControl.Interview.SeniorDeveloper.Model/VehicleModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace GroundControl.Interview.SeniorDeveloper.Model
 {
     public class VehicleModel
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
     }
 }
