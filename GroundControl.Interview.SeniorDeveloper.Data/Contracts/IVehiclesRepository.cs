e8e46f59f55c5371f89c5243ae38b285ac51147d
Conor Maher <conor-995@hotmail.co.uk>
2021-07-27 17:09:38 +0100

    Data Project Skeleton
    - Add reference to API project
    - Add contracts and repository
    - WIP

---
+++ GroundControl.Interview.SeniorDeveloper.Data/Contracts/IVehiclesRepository.cs
@@ -0,0 +1,13 @@
+﻿using GroundControl.Interview.SeniorDeveloper.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroundControl.Interview.SeniorDeveloper.Data.Contracts
+{
+    public interface IVehiclesRepository
+    {
+        public Task<IEnumerable<VehicleMake>> GetMakesAsync();
+
+        public Task<IEnumerable<VehicleModel>> GetModelsByMakeAsync(int makeId);
+    }
+}
---
+++ GroundControl.Interview.SeniorDeveloper.Data/Repositories/DapperVehiclesRepository.cs
@@ -0,0 +1,22 @@
+﻿using GroundControl.Interview.SeniorDeveloper.Data.Contracts;
+using GroundControl.Interview.SeniorDeveloper.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroundControl.Interview.SeniorDeveloper.Data.Repositories
+{
+    public class DapperVehiclesRepository : IVehiclesRepository
+    {
+        // Use DTO models for the data layer and implement a mapper to the API models, if time near the end
+
+        public Task<IEnumerable<VehicleMake>> GetMakesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<VehicleModel>> GetModelsByMakeAsync(int makeId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
