f04bc299acf6a55e301187097f95dbdfe4d4f272
Conor Maher <conor-995@hotmail.co.uk>
2021-07-27 17:32:40 +0100

    Implement SP Calls via Dapper
    - Call the relevant SPs for each function
    - Change const strings to readonly as SPs are subject to change long-term

--- GroundControl.Interview.SeniorDeveloper.Data/Repositories/DapperVehiclesRepository.cs
+++ GroundControl.Interview.SeniorDeveloper.Data/Repositories/DapperVehiclesRepository.cs
@@ -1,4 +1,5 @@
-﻿using GroundControl.Interview.SeniorDeveloper.Data.Contracts;
+﻿using Dapper;
+using GroundControl.Interview.SeniorDeveloper.Data.Contracts;
 using GroundControl.Interview.SeniorDeveloper.Model;
 using System.Collections.Generic;
 using System.Data;
@@ -11,22 +12,30 @@ namespace GroundControl.Interview.SeniorDeveloper.Data.Repositories
         // Use DTO models for the data layer and implement a mapper to the API models, if time near the end
         private readonly IDbConnection _dbConnection;
 
-        private const string SP_GETALLMAKES = "Usp_Vehicle_GetAllMakes";
-        private const string SP_GETMODELSBYMAKE = "Usp_Vehicle_GetModelsByMake";
+        private readonly string Sp_GetAllMakes = "Usp_Vehicle_GetAllMakes";
+        private readonly string Sp_GetModelsByMake = "Usp_Vehicle_GetModelsByMake";
 
         public DapperVehiclesRepository(IDbConnection dbConnection)
         {
             _dbConnection = dbConnection;
         }
 
-        public Task<IEnumerable<VehicleMake>> GetMakesAsync()
+        // Todo if time - error handling around results/data access for these two functions
+        public async Task<IEnumerable<VehicleMake>> GetMakesAsync()
         {
-            throw new System.NotImplementedException();
+            var results = await _dbConnection.QueryAsync<VehicleMake>(Sp_GetAllMakes, CommandType.StoredProcedure);
+
+            return results;
         }
 
-        public Task<IEnumerable<VehicleModel>> GetModelsByMakeAsync(int makeId)
+        public async Task<IEnumerable<VehicleModel>> GetModelsByMakeAsync(int makeId)
         {
-            throw new System.NotImplementedException();
+            var parameters = new DynamicParameters();
+            parameters.Add("@MakeId", makeId);
+
+            var results = await _dbConnection.QueryAsync<VehicleModel>(Sp_GetModelsByMake, parameters, commandType: CommandType.StoredProcedure);
+
+            return results;
         }
     }
 }
